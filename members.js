d3ca4cbf6eee0f12f565d8c298c9c174f5674fe0
Jeremy <jeremy.s.sutton@gmail.com>
2013-11-07 01:04:26 -0500

    filter out portraits by state

--- members.js
+++ members.js
@@ -1,6 +1,69 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+var states = {
+	'AL' : 'ALABAMA',
+    'AK' : 'ALASKA',
+    'AS' : 'AMERICAN SAMOA',
+    'AZ' : 'ARIZONA',
+    'AR' : 'ARKANSAS',
+    'CA' : 'CALIFORNIA',
+    'CO' : 'COLORADO',
+    'CT' : 'CONNECTICUT',
+    'DE' : 'DELAWARE',
+    'DC' : 'DISTRICT OF COLUMBIA',
+    'FM' : 'FEDERATED STATES OF MICRONESIA',
+    'FL' : 'FLORIDA',
+    'GA' : 'GEORGIA',
+    'GU' : 'GUAM',
+    'HI' : 'HAWAII',
+    'ID' : 'IDAHO',
+    'IL' : 'ILLINOIS',
+    'IN' : 'INDIANA',
+    'IA' : 'IOWA',
+    'KS' : 'KANSAS',
+    'KY' : 'KENTUCKY',
+    'LA' : 'LOUISIANA',
+    'ME' : 'MAINE',
+    'MH' : 'MARSHALL ISLANDS',
+    'MD' : 'MARYLAND',
+    'MA' : 'MASSACHUSETTS',
+    'MI' : 'MICHIGAN',
+    'MN' : 'MINNESOTA',
+    'MS' : 'MISSISSIPPI',
+    'MO' : 'MISSOURI',
+    'MT' : 'MONTANA',
+    'NE' : 'NEBRASKA',
+    'NV' : 'NEVADA',
+    'NH' : 'NEW HAMPSHIRE',
+    'NJ' : 'NEW JERSEY',
+    'NM' : 'NEW MEXICO',
+    'NY' : 'NEW YORK',
+    'NC' : 'NORTH CAROLINA',
+    'ND' : 'NORTH DAKOTA',
+    'MP' : 'NORTHERN MARIANA ISLANDS',
+    'OH' : 'OHIO',
+    'OK' : 'OKLAHOMA',
+    'OR' : 'OREGON',
+    'PW' : 'PALAU',
+    'PA' : 'PENNSYLVANIA',
+    'PR' : 'PUERTO RICO',
+    'RI' : 'RHODE ISLAND',
+    'SC' : 'SOUTH CAROLINA',
+    'SD' : 'SOUTH DAKOTA',
+    'TN' : 'TENNESSEE',
+    'TX' : 'TEXAS',
+    'UT' : 'UTAH',
+    'VT' : 'VERMONT',
+    'VI' : 'VIRGIN ISLANDS',
+    'VA' : 'VIRGINIA',
+    'WA' : 'WASHINGTON',
+    'WV' : 'WEST VIRGINIA',
+    'WI' : 'WISCONSIN',
+    'WY' : 'WYOMING'
+};
+
+
 exports.getCongressmen = function( stateCode, callback ) {
 	callback = (callback || function(){});
 	var members = { senators: [], reps: [] };
@@ -73,9 +136,6 @@ exports.getSenatePortraits = function( stateCode, callback ) {
 	request({
 		uri: "http://en.wikipedia.org/wiki/List_of_current_United_States_Senators",
 		method: "GET",
-		qs: {
-			state: stateCode
-		},
 		timeout: 10000,
 		followRedirect: true
 	}, function(err, response, body) {
@@ -91,14 +151,22 @@ exports.getSenatePortraits = function( stateCode, callback ) {
 			state = $(img).parent().parent().prev().prev().children().text();
 			name = $(img).parent().parent().next().children().children().text();
 
-			portraits.push({
-				"name" : name,
-				"imgurl" : "http:" + imgurl,
-				"state" : state,
-			});
+			console.log("state: " + state + " : stateCode: " + stateCode.toUpperCase());
+			if (state.toUpperCase() == states[stateCode.toUpperCase()]) {
+				portraits.push({
+					"name" : name,
+					"imgurl" : "http:" + imgurl,
+					"state" : state,
+				});
+			}
 		});
 
-		callback( portraits );
+		if (portraits.length > 0) 
+			callback( portraits );
+		else
+			callback( "Invalid state." );
+
+		
 		return( this );
 	});
 };
